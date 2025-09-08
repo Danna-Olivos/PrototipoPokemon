803090ca328813362452ab8be72e674674d9301c
brenda96011 <brendarj1124@gmail.com>
2025-09-07 18:22:38 -0600

    borrador de clase Cuenta(?

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -38,7 +38,7 @@ public Class Cuenta{
     }
 
     public String getEquipo(){
-        return idCodigo;
+        return equipo;
     }
     public void setEquipo(String equipo){
         this.equipo = equipo;
@@ -54,13 +54,13 @@ public Class Cuenta{
 
         try (FileWriter fw = new FileWriter(archivo, true);
             BufferedWriter bw = new BufferedWriter(fw)){
-            if (!archivo.exists()) {
+            if(!archivo.exists()){
                 bw.write(encabezado);
                 bw.newLine();
             }
             bw.write(c.toString());
             bw.newLine();
-        } catch (IOException e){
+        } catch(IOException e){
             System.out.println("Hubo un error u.u");
             e.printStackTrace();
         }
@@ -68,21 +68,21 @@ public Class Cuenta{
 
     public static String consultarCuenta(String llaveCodigo){
         File archivo = new File("cuentas.csv");
-        if (!archivo.exists()) {
+        if(!archivo.exists()){
             System.out.println("Aún no hay cuentas almacenadas D:");
             return null;
         }
 
-        try (FileReader fw = new FileReader(archivo);
-            BufferedReader br = new BufferedReader(fw)){
+        try (FileReader fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr)){
             String linea = br.readLine(); // primera linea leida: el encabezado
-            while ((linea = br.readLine()) != null) {
+            while((linea = br.readLine()) != null){
                 String llaveActual = linea.split(",")[0];
-                if (llaveActual.equals(llaveCodigo)) {
+                if(llaveActual.equals(llaveCodigo)){
                     return encabezado + "\n" + linea; // encontrada, se devuelve la info
                 }
             }
-        } catch (IOException e) {
+        } catch(IOException e){
             System.out.println("Hubo un error u.u");
             e.printStackTrace();
         }
@@ -90,12 +90,87 @@ public Class Cuenta{
         return null;
     }
 
-    public static void editarCuenta(que se le pone aqui, igual la llave? D:){
-        ewoiigfhksijifikskksksksk
+    public static void editarCuenta(Cuenta c){
+        File archivo = new File("cuentas.csv");
+        if(!archivo.exists()){
+            System.out.println("Aún no hay cuentas almacenadas D:");
+            return;
+        }
+
+        List<String> listaLineas = new ArrayList<>(); // la creamos pq cuando tendremos q volver a escribir todo el csv
+
+        try (FileReader fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr)){
+            listaLineas.add(br.readLine()); // primera linea leida: el encabezado
+            String linea;
+            while((linea = br.readLine()) != null){
+                String llaveActual = linea.split(",")[0];
+                if(llaveActual.equals(c.getLlaveCodigo())){
+                    listaLineas.add(c.toString());
+                } else{
+                    listaLineas.add(linea);
+                }
+            }
+        } catch(IOException e){
+            System.out.println("Hubo un error u.u");
+            e.printStackTrace();
+        }
+
+        try (FileWriter fw = new FileWriter(archivo);
+            BufferedWriter bw = new BufferedWriter(fw)){
+            for(String linea : listaLineas){
+                bw.write(linea);
+                bw.newLine();
+            }
+        } catch(IOException e){
+            System.out.println("Hubo un error u.u");
+            e.printStackTrace();
+        }
     } 
 
-    public static void eliminarCuenta(){
-        //TODOS
+    public static void eliminarCuenta(Cuenta c){
+        File archivo = new File("cuentas.csv");
+        if(!archivo.exists()){
+            System.out.println("Aún no hay cuentas almacenadas D:");
+            return;
+        }
+
+        List<String> listaLineas = new ArrayList<>(); // la creamos pq cuando tendremos q volver a escribir todo el csv
+
+        try (FileReader fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr)){
+            boolean encontrada = false;
+            listaLineas.add(br.readLine()); // primera linea leida: el encabezado
+            String linea;
+            while((linea = br.readLine()) != null){
+                String llaveActual = linea.split(",")[0];
+                if(llaveActual.equals(c.getLlaveCodigo())){
+                    encontrada = true;
+                } else{
+                    listaLineas.add(linea);
+                }
+            }
+
+            if(!encontrada){
+                System.out.println("Esa cuenta ni siquiera estaba reistrada.");
+                return;
+            }
+
+        } catch(IOException e){
+            System.out.println("Hubo un error u.u");
+            e.printStackTrace();
+        }
+
+        try (FileWriter fw = new FileWriter(archivo);
+            BufferedWriter bw = new BufferedWriter(fw)){
+            for(String linea : listaLineas){
+                bw.write(linea);
+                bw.newLine();
+            }
+        } catch(IOException e){
+            System.out.println("Hubo un error u.u");
+            e.printStackTrace();
+        }
     }
 
 
