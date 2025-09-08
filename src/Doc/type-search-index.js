3238cbe8d6c867a4e793793ce702facac1fb0edd
brenda96011 <brendarj1124@gmail.com>
2025-09-08 17:41:53 -0600

    documantaci b

---
+++ src/Doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cuenta"},{"p":"<Unnamed>","l":"Participantes"},{"p":"<Unnamed>","l":"Pokemon"},{"p":"<Unnamed>","l":"SolrockBattleSystem"}];updateSearchResults();
